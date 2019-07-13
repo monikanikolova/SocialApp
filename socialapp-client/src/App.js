b6f8286f06595486fe80d76d509025257b9513b6
monikanikolova <monika.nikolova@yahoo.com>
2019-07-13 12:22:03 -0400

    react app created

---
+++ socialapp-client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
+
+
+
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <Router>
+           <Switch>
+              <Route exact path='/' component={home} />
+              <Route exact path='/login' component={login} />
+              <Route exact path='/signup' component={signup} />
+           </Switch>
+        </Router>
+
+      </div>
+    )
+  }
+}
+
+export default App;
